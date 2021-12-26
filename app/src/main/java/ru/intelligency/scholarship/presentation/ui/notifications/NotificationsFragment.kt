decdae64756874fbce32d02274b31caaaa261e84
Maxim Zadumin <recentfear@yandex.ru>
2021-12-26 23:52:57 +0500

    Fixed notification fragment

--- app/src/main/java/ru/intelligency/scholarship/presentation/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/ru/intelligency/scholarship/presentation/ui/notifications/NotificationsFragment.kt
@@ -1,10 +1,10 @@
 package ru.intelligency.scholarship.presentation.ui.notifications
 
 import ru.intelligency.scholarship.R
-import ru.intelligency.scholarship.databinding.FragmentMyApplicationsBinding
+import ru.intelligency.scholarship.databinding.FragmentNotificationsBinding
 import ru.intelligency.scholarship.presentation.base.BaseFragment
 
-class NotificationsFragment : BaseFragment<FragmentMyApplicationsBinding>() {
+class NotificationsFragment : BaseFragment<FragmentNotificationsBinding>() {
 
     override fun getLayoutId(): Int {
         return R.layout.fragment_notifications
