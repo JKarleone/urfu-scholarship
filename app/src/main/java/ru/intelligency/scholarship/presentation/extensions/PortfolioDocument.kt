d0472fced1f930894b41694e5d00771b22de2a58
Maxim Zadumin <recentfear@yandex.ru>
2022-01-15 02:15:45 +0500

    Fixed document's expiration date

--- app/src/main/java/ru/intelligency/scholarship/presentation/extensions/PortfolioDocument.kt
+++ app/src/main/java/ru/intelligency/scholarship/presentation/extensions/PortfolioDocument.kt
@@ -4,13 +4,17 @@ import android.content.Context
 import ru.intelligency.scholarship.R
 import ru.intelligency.scholarship.presentation.ui.portfolio.model.PortfolioDocument
 import ru.intelligency.scholarship.presentation.utils.Status
+import java.util.Calendar
 
 fun PortfolioDocument.getStatusText(context: Context): String {
     return when (documentStatus) {
         Status.ACCEPTED -> {
             context.getString(
                 R.string.portfolio_profile_expiration_date,
-                expirationDate.getStringDate()
+                Calendar.getInstance().apply {
+                    timeInMillis = expirationDate
+                    set(Calendar.YEAR, get(Calendar.YEAR) + 2)
+                }.timeInMillis.getStringDate()
             )
         }
         Status.IN_WAITING -> {
